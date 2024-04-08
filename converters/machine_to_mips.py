de3a2498b2ea806455ef9671f88afa7f8048eb5e
jay2044 <117245060+jay2044@users.noreply.github.com>
2024-04-08 16:32:22 -0700

    unit testing

--- converters/machine_to_mips.py
+++ converters/machine_to_mips.py
@@ -1,48 +1,47 @@
-import mips_instructions
+from mips_instructions import *
 
-# testing variable 
+# testing variable
 string = "00110101010010010000000000100101"
+
 
 # r type conversion
 def rTypeConversion(machineCode, opcode, funct):
-    for name, value in instructionType.instruction_dictionary.items():
-        if funct != -1 and funct == int(value[1],16):
-            rs = instructionType.getRegisterName(int(machineCode[6:11],2))
-            rt = instructionType.getRegisterName(int(machineCode[11:16],2))
-            rd = instructionType.getRegisterName(int(machineCode[16:21],2))
-            shamt = int(machineCode[21:26],2)
-            
-            instruction = instructionType.rTypeInstruction(opcode,rs,rt,rd,shamt,funct)
-            print(f"{name} {rs} {rt} {rd}")
-            break
+    for name, value in instruction_dictionary.items():
+        if funct != -1 and funct == int(value[1], 16):
+            rs = get_register_name(int(machineCode[6:11], 2))
+            rt = get_register_name(int(machineCode[11:16], 2))
+            rd = get_register_name(int(machineCode[16:21], 2))
+            shamt = int(machineCode[21:26], 2)
+
+            # instruction = rTypeInstruction(opcode,rs,rt,rd, funct)
+            return f"{name} {rs} {rt} {rd}"
+
 
 # i type and jtype conversion
 def eitherITypeORJType(machineCode, opcode):
-    for name, value in instructionType.instruction_dictionary.items():
-        if isinstance(value, str) and opcode == int(value,16):
-            type = instructionType.typeOfInstruction(name)
-            
-            if type == 'itype':
-                rs = instructionType.getRegisterName(int(machineCode[6:11],2))
-                rt = instructionType.getRegisterName(int(machineCode[11:16],2))
-                immediate = int(machineCode[16:],2)
-                instruction = instructionType.iTypeInstruction(opcode,rs,rt,immediate)
-                print(f"{name} {rt} {rs} {immediate}")
-                break
-            else:
-                address = int(machineCode[5:],2)
-                instruction = instructionType.jTypeInstruction(opcode, address)
-                print(f"{name} {hex(address)}")
-                break
+    for name, value in instruction_dictionary.items():
+        if isinstance(value, str) and opcode == int(value, 16):
+            type = type_of_instruction(name)
 
-# USE THIS FUNCTION TO USE THE FILE
+            if type == 'itype':
+                rs = get_register_name(int(machineCode[6:11], 2))
+                rt = get_register_name(int(machineCode[11:16], 2))
+                immediate = int(machineCode[16:], 2)
+                # instruction = iTypeInstruction(opcode,rs,rt,immediate)
+                return f"{name} {rt} {rs} {immediate}"
+            else:
+                address = int(machineCode[5:], 2)
+                # instruction = jTypeInstruction(opcode, address)
+                return f"{name} {hex(address)}"
+
+            # USE THIS FUNCTION TO USE THE FILE
+
+
 def checkForOpcode(machinecode):
-    opcode = int(machinecode[0:6],2)
+    opcode = int(machinecode[0:6], 2)
     funct = -1
     if opcode == 0:
-        funct = int(machinecode[26:],2)
-        rTypeConversion(machinecode, opcode, funct)
+        funct = int(machinecode[26:], 2)
+        return rTypeConversion(machinecode, opcode, funct)
     else:
-        eitherITypeORJType(machinecode, opcode)
-
-checkForOpcode(string)
+        return eitherITypeORJType(machinecode, opcode)
