e71f96bee504229a5bb5606bfa43a863d5e2b513
jay2044 <117245060+jay2044@users.noreply.github.com>
2024-04-08 17:14:13 -0700

    test

---
+++ Unit Testing/test_mips_to_machine.py
@@ -0,0 +1,82 @@
+from converters.mips_to_machine import mips_to_machine
+
+# Test examples:
+r_type_test_examples = [
+    ["add $t1, $t2, $t3", "00000001010010110100100000100000"],  # add
+    ["addu $t1, $t2, $t3", "00000001010010110100100000100001"],  # addu
+    ["and $t1, $t2, $t3", "00000001010010110100100000100100"],  # and
+    ["jr $ra", "00000011111000000000000000001000"],  # jr
+    ["nor $t1, $t2, $t3", "00000001010010110100100000100111"],  # nor
+    ["or $t1, $t2, $t3", "00000001010010110100100000100101"],  # or
+    ["slt $t1, $t2, $t3", "00000001010010110100100000101010"],  # slt
+    ["sltu $t1, $t2, $t3", "00000001010010110100100000101011"],  # sltu
+    ["sll $t1, $t2, 2", "00000000000010100100100010000000"],  # sll
+    ["srl $t1, $t2, 2", "00000000000010100100100010000010"],  # srl
+    ["sub $t1, $t2, $t3", "00000001010010110100100000100010"],  # sub
+    ["subu $t1, $t2, $t3", "00000001010010110100100000100011"],  # subu
+    ["div $t1, $t2", "00000001001010100000000000101010"],  # div
+    ["divu $t1, $t2", "00000001001010100000000000101011"],  # divu
+    ["mfhi $t1", "00000000000000000100100000010000"],  # mfhi
+    ["mflo $t1", "00000000000000000100100000010010"],  # mflo
+    ["mfc0 $t1, $0", "01000000000010010000000000000000"],  # mfc0, note: coprocessor 0 instruction
+    ["mult $t1, $t2", "00000001001010100000000000011000"],  # mult
+    ["multu $t1, $t2", "00000001001010100000000000011001"],  # multu
+    ["sra $t1, $t2, 2", "00000000000010100100100000000011"]  # sra
+]
+
+i_type_test_examples = [
+    ["addi $t1, $t2, 10", "00100001010010010000000000001010"],  # addi
+    ["addiu $t1, $t2, 10", "00100101010010010000000000001010"],  # addiu
+    ["andi $t1, $t2, 15", "00110001010010010000000000001111"],  # andi
+    ["beq $t1, $t2, 16", "00010001001010000000000000010000"],  # beq
+    ["bne $t1, $t2, 16", "00010101001010000000000000010000"],  # bne
+    ["lbu $t1, 32($t2)", "10010001010010010000000000100000"],  # lbu
+    ["lhu $t1, 32($t2)", "10010101010010010000000000100000"],  # lhu
+    ["ll $t1, 32($t2)", "11000001010010010000000000100000"],  # ll (Load Linked)
+    ["lui $t1, 1024", "00111100000010010000010000000000"],  # lui
+    ["lw $t1, 32($t2)", "10001101010010010000000000100000"],  # lw
+    ["ori $t1, $t2, 255", "00110101010010011111111111111111"],  # ori
+    ["slti $t1, $t2, -10", "00101001010010011111111111110110"],  # slti
+    ["sltiu $t1, $t2, 10", "00101101010010010000000000001010"],  # sltiu
+    ["sb $t1, 32($t2)", "10100001010010010000000000100000"],  # sb
+    ["sc $t1, 32($t2)", "11100001010010010000000000100000"],  # sc (Store Conditional)
+    ["sh $t1, 32($t2)", "10100101010010010000000000100000"],  # sh
+    ["sw $t1, 32($t2)", "10101101010010010000000000100000"]  # sw
+]
+
+j_type_test_examples = [
+    ["j 0x00400000", "00001000000000010000000000000000"],  # j
+    ["jal 0x00400000", "00001100000000010000000000000000"],  # jal
+    ["jr $ra", "00000011111000000000000000001000"]  # jr
+]
+
+
+def test_mips_to_machine():
+    # Combine all test cases into a single list for easier processing
+    all_test_cases = r_type_test_examples + i_type_test_examples + j_type_test_examples
+
+    # Track the number of failed tests
+    failed_tests = 0
+
+    for test_case in all_test_cases:
+        mips_instruction, expected_machine_code = test_case
+        try:
+            received_machine_code = mips_to_machine(mips_instruction)
+        except Exception as e:
+            print(f"Error processing instruction: {mips_instruction}")
+            failed_tests += 1
+            continue
+
+        if received_machine_code != expected_machine_code:
+            mnemonic = mips_instruction.split()[0]
+            print(f"Failed test for: {mnemonic.ljust(20)} expected: {expected_machine_code.ljust(10)} received: {received_machine_code}")
+
+            failed_tests += 1
+
+    if failed_tests == 0:
+        print("All tests passed successfully.")
+    else:
+        print(f"{failed_tests} tests failed.")
+
+
+test_mips_to_machine()
--- converters/mips_to_machine.py
+++ converters/mips_to_machine.py
@@ -16,7 +16,7 @@ def mips_to_machine(instruction):
             else:
                 shamt = '00000'
             machine = r_type_instruction(mnemonic, rs, rt, rd, shamt)
-            print(f"{machine.opcode.zfill(6)} {rs} {rt} {rd} {shamt} {machine.funct.zfill(6)}")
+            return f"{machine.opcode.zfill(6)}{rs}{rt}{rd}{shamt}{machine.funct.zfill(6)}"
 
         else:
             rs = bin(named_registers[instruction[2]])[2:].zfill(5)
@@ -24,7 +24,7 @@ def mips_to_machine(instruction):
             rd = bin(named_registers[instruction[1]])[2:].zfill(5)
             shamt = '00000'
             machine = r_type_instruction(mnemonic, rs, rt, rd, shamt)
-            print(f"{machine.opcode.zfill(6)} {rs} {rt} {rd} {shamt} {machine.funct.zfill(6)}")
+            return f"{machine.opcode.zfill(6)}{rs}{rt}{rd}{shamt}{machine.funct.zfill(6)}"
 
     elif instruction_type == 'itype':
         if mnemonic in ['lw', 'sw']:
@@ -39,107 +39,9 @@ def mips_to_machine(instruction):
             rs = bin(named_registers[instruction[2]])[2:].zfill(5)
             immediate = bin(int(instruction[3], 0))[2:].zfill(16)
         machine = i_type_instruction(mnemonic, rs, rt, immediate)
-        print(f"{machine.opcode.zfill(6)} {rs} {rt} {immediate}")
+        return f"{machine.opcode.zfill(6)}{rs}{rt}{immediate}"
 
     elif instruction_type == 'jtype':
         address = bin(int(instruction[1], 16))[2:].zfill(26)
         machine = j_type_instruction(mnemonic, address)
-        print(f"{machine.opcode.zfill(6)} {machine.address}")
-
-
-mips_to_machine("j 0x3c")
-
-
-# Test examples:
-'''
-Instruction: add $t1, $t2, $t3
-Machine Code: 000000 01010 01011 01001 00000 100000
-
-Instruction: sub $t1, $t2, $t3
-Machine Code: 000000 01010 01011 01001 00000 100010
-
-Instruction: and $t1, $t2, $t3
-Machine Code: 000000 01010 01011 01001 00000 100100
-
-Instruction: or $t1, $t2, $t3
-Machine Code: 000000 01010 01011 01001 00000 100101
-
-Instruction: slt $t1, $t2, $t3
-Machine Code: 000000 01010 01011 01001 00000 101010
-
-Instruction: sll $t1, $t2, 2
-Machine Code: 000000 00000 01010 01001 00010 000000
-
-Instruction: sw $t1, 32($t2)
-Machine Code: 101011 01010 01001 00000 00000 100000
-
-Instruction: lw $t1, 32($t2)
-Machine Code: 100011 01010 01001 00000 00000 100000
-
-Instruction: beq $t1, $t2, label
-Machine Code: 000100 01001 01010 label offset
-
-Instruction: j target
-Machine Code: 000010 target address
-
-Instruction: jr $t1
-Machine Code: 000000 01001 00000 00000 00000 001000
-
-Instruction: jal target
-Machine Code: 000011 target address
-
-Instruction: nor $t1, $t2, $t3
-Machine Code: 000000 01010 01011 01001 00000 100111
-
-Instruction: xor $t1, $t2, $t3
-Machine Code: 000000 01010 01011 01001 00000 100110
-
-Instruction: srl $t1, $t2, 2
-Machine Code: 000000 00000 01010 01001 00010 000010
-
-Instruction: sra $t1, $t2, 2
-Machine Code: 000000 00000 01010 01001 00010 000011
-
-Instruction: lui $t1, imm
-Machine Code: 001111 00000 01001 immediate
-
-Instruction: bne $t1, $t2, label
-Machine Code: 000101 01001 01010 label offset
-
-Instruction: addi $t1, $t2, 10
-Machine Code: 001000 01010 01001 0000 0000 0000 1010
-
-Instruction: slti $t1, $t2, -5
-Machine Code: 001010 01010 01001 1111 1111 1111 1011
-
-Instruction: andi $t1, $t2, 15
-Machine Code: 001100 01010 01001 0000 0000 0000 1111
-
-Instruction: ori $t1, $t2, 255
-Machine Code: 001101 01010 01001 0000 0000 1111 1111
-
-Instruction: xori $t1, $t2, 16
-Machine Code: 001110 01010 01001 0000 0000 0001 0000
-
-Instruction: lb $t1, 100($t2)
-Machine Code: 100000 01010 01001 0000 0110 0100 0000
-
-Instruction: sb $t1, 100($t2)
-Machine Code: 101000 01010 01001 0000 0110 0100 0000
-
-Instruction: lh $t1, 2($t2)
-Machine Code: 100001 01010 01001 0000 0000 0000 0010
-
-Instruction: sh $t1, 2($t2)
-Machine Code: 101001 01010 01001 0000 0000 0000 0010
-
-Instruction: bgtz $t1, 8
-Machine Code: 000111 01001 00000 0000 0000 0000 1000
-
-Instruction: blez $t1, -4
-Machine Code: 000110 01001 00000 1111 1111 1111 1100
-
-Instruction: lui $t1, 1024
-Machine Code: 001111 00000 01001 0000 0100 0000 0000
-'''
-
+        return f"{machine.opcode.zfill(6)}{machine.address}"
