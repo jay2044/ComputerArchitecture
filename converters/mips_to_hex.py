ecfaed245d47426e33daf2872a8b9d3e904d5625
derek <derektran2424@gmail.com>
2024-05-11 02:56:25 -0700

    I think should be complete for now. Will add to it if requested

--- converters/mips_to_hex.py
+++ converters/mips_to_hex.py
@@ -122,9 +122,6 @@ def mips_to_hex():
 
     instruction_in_binary = mips_to_machine(instruction)
 
-
-    # checks to see if the user input is correct in integer form.
-    print(int(instruction_in_binary,2))
     try: 
         assert int(input(f"Enter the hex for this instruction: {instruction}\n"), 16) == int(instruction_in_binary,2)
         print("CORRECT!!!")
