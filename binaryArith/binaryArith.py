358ef4bf5a27668648ad974afe22e6ed458b47b6
giopdev <giovanni.p.paladino@gmail.com>
2024-05-13 14:38:55 -0700

    division algorithm working for unsigned

--- binaryArith/binaryArith.py
+++ binaryArith/binaryArith.py
@@ -92,23 +92,17 @@ def multAlg(multd, multr, twoscomp):
 
 # binary division algorithm
 def divAlg(dividend, divisor, twoscomp):
-    quotient = ""
-    problem_values = [dividend, divisor, quotient]
-    
 
-    #this is here because if the result of division is 0 then the remainder is the divisor, which wasn't kept
-    #this needs to be cleaned up somehow, maybe when each value of the question is stored in an array
+    # this is here because if the result of division is 0 then the remainder is the divisor, which wasn't kept
+    # this needs to be cleaned up somehow, maybe when each value of the question is stored in an array
     original = divisor
-    padSize = len(dividend)
-    j = 0
 
-    if j == len(divisor):
-        return
+    # padSize = len(dividend)
+    # j = 0
 
-    # while j != len(divisor) and divisor[j] != "1":
-    #     j += 1
-    # j += 1
-    
+    # if len(divisor) == 0:
+    #     return
+
     if twoscomp:
         # bool values to check if multd and multr are 2's comp
         ddComp = False
@@ -117,70 +111,83 @@ def divAlg(dividend, divisor, twoscomp):
         # check for leading bit 1, if 2's comp numbers
         if dividend[0] == "1":
             ddComp = True
-            #dividend = twosComp(dividend, n)
+            dividend = twosComp(dividend, len(dividend))
 
         if divisor[0] == "1":
             drComp = True
-            #divisor = twosComp(divisor, m)
+            divisor = twosComp(divisor, len(divisor))
 
-    # right justify divisor
-    # for i in range(m):
-    #     divisor = divisor + "0"
-    size = len(divisor)
+    # left justify divisor
+    original_divisor_length = len(divisor)
     divisor = divisor + "0" * len(divisor)
 
-    # setting the new size of divisor
-    # m = len(divisor)
-    #size = len(divisor)
-    # j = len(divisor) - j
-    
-    for i in range(size + 1):
+    dividend = ("0" * len(original)) + dividend
+
+    # quotient = max bit size with all bits unset
+    quotient = "0" * original_divisor_length
+
+    problem_values = [[dividend, divisor, quotient]]
+
+    problem_values.append(copy.copy(problem_values[0])) # Initialization step
+
+    for i in range(original_divisor_length + 1):
+        original_remainder = copy.copy(problem_values[i+1][0])
 
         # remainder = remainder - divisor
-        dividend = binaryDiff(dividend, divisor)
+        problem_values[i+1][0] = binaryDiff(problem_values[i+1][0], problem_values[i+1][1])
 
-        # cond dividend[0] == '1'
-        if dividend[0] == "-":
+        if problem_values[i+1][0][0] == "-": # if remainder < 0 => change remainder back to original, sll quotient
             # rem += divisor
-            dividend = binarySum(dividend, divisor)
-            quotient += "0"
-        else:
-            quotient += "1"
+            problem_values[i+1][0] = original_remainder
+            # dividend = binarySum(dividend, divisor)
+            problem_values[i+1][2] = problem_values[i][2][1:]
+            problem_values[i+1][2] += "0"
+            # quotient += "0"
+        else: # if remainder > 0 => keep remainder the same, pad out to proper size, sll quotient and make quotient[0] = 1
+            problem_values[i+1][0] = problem_values[i+1][0][2:]
+            problem_values[i+1][0] = "0" * ((original_divisor_length * 2) - len(problem_values[i+1][0])) + problem_values[i+1][0]
+
+            problem_values[i+1][2] = problem_values[i][2][1:]
+            problem_values[i+1][2] += "1"
 
         # shift divisor right
-        divisor = divisor[:-1]
-        
-        #size = size - 1
-        i +=1
+        shift_string = problem_values[i+1][1][:-1]
+        problem_values[i+1][1] = "0" + shift_string
+        # append next step
+        problem_values.append(copy.copy(problem_values[i+1]))
 
-    #this should return an array of all the values needed to give the question
-    #should be padded in a similar manner to the dividend
-    problem_values[2] = quotient
-    if padSize - len(problem_values[2]) > 0:
-        problem_values[2] = "0" * (padSize - len(problem_values[2])) + problem_values[2]
-    elif padSize - len(problem_values[2]) < 0:
-        problem_values[2] = problem_values[2][abs(padSize - len(problem_values[2])):]
-    
-    if problem_values[2] == "0" * padSize:
-        problem_values[0] = original
-    else:
-        problem_values[0] = dividend[2:]
+    # quotient padding, pads up or down to length
+    # problem_values[2] = quotient
+    # if padSize - len(problem_values[0][2]) > 0:
+    #     problem_values[2] = "0" * (padSize - len(problem_values[2])) + problem_values[2]
+    # elif padSize - len(problem_values[2]) < 0:
+    #     problem_values[2] = problem_values[2][abs(padSize - len(problem_values[2])):]
 
-        if padSize - len(problem_values[0]) > 0:
-            problem_values[0] = "0" * (padSize - len(problem_values[0])) + problem_values[0]
 
-    #should always be 1??
-    problem_values[1] = divisor[-1:]
+    # # dividend padding
+    # if problem_values[2] == "0" * padSize:
+    #     problem_values[0] = original
+    # else:
+    #     problem_values[0] = dividend[2:]
 
-    #should be padded in a similar manner to the dividend
-    problem_values[2] = quotient
-    if padSize - len(problem_values[2]) > 0:
-        problem_values[2] = "0" * (padSize - len(problem_values[2])) + problem_values[2]
-    elif padSize - len(problem_values[2]) < 0:
-        problem_values[2] = problem_values[2][abs(padSize - len(problem_values[2])):]
-    
-    #add something here for 2's comp??
+    #     if padSize - len(problem_values[0]) > 0:
+    #         problem_values[0] = "0" * (padSize - len(problem_values[0])) + problem_values[0]
+
+    # # divisor padding, idk what this value should be or mean
+    # problem_values[1] = divisor[-1:]
+
+    # add something here for 2's comp??
+    if twoscomp:
+        # match remainder to sign of dividend
+        if ddComp:
+            problem_values[0] = twosComp(problem_values[0], len(problem_values[0]))
+
+            # handle if dividend and divisor have different signs
+            if not drComp:
+                problem_values[2] = twosComp(problem_values[2], len(problem_values[2]))
 
     return problem_values
 
-print(divAlg("0001", "0101", False))
+
+print(divAlg("0111", "0010", False))
+
